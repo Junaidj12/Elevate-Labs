6237ffa08445c8e4ce558db5c4bfbf5390b5c141
Muhammad junaid Jamadar <120266626+Junaidj12@users.noreply.github.com>
2025-09-22 17:01:26 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    // Method for Addition
+ 
     public static double add(double a, double b) {
         return a + b;
     }
 
-    // Method for Subtraction
+  
     public static double subtract(double a, double b) {
         return a - b;
     }
 
-    // Method for Multiplication
+    
     public static double multiply(double a, double b) {
         return a * b;
     }
 
-    // Method for Division (with divide by zero handling)
+  
     public static double divide(double a, double b) {
         if (b == 0) {
             System.out.println("Error: Division by zero is not allowed.");
